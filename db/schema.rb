d668ef44e8668bd0ebd635173f8c555dec0c4089
Hashir <dev@macs-MacBook-Pro.local>
2022-11-06 23:01:03 -0800

    User: basic crud and model

---
+++ db/schema.rb
@@ -0,0 +1,16 @@
+ActiveRecord::Schema.define(version: 2022_11_06_130052) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
